664391103bf911ea7dfc40aa2260b658ef00bef2
tomasz solarz <tomasz.solarz@gmail.com>
2017-12-30 17:38:21 +0100

    Initial commit

---
+++ ImplementingIDisposable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ImplementingIDisposable i = new ImplementingIDisposable())
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class ImplementingIDisposable : IDisposable
+    {
+        public override string ToString()
+        {
+            return "ToString method was called";
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose method was called");
+        }
+    }
+}
