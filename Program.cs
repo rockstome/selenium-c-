85c45b9ef7a358f5087fa3b19d08c632bc3b91b7
tomasz solarz <tomasz.solarz@gmail.com>
2017-12-31 10:33:54 +0100

    new files

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningProject
+{
+    public class Program
+    {
+        static int Main(string[] args)
+        {
+
+            return 0;
+        }
+    }
+}
